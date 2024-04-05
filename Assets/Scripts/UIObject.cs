cc71f74ec83ff7b4d9bf56ae6d79a74f6167ef8f
pilho2961 <149761165+pilho2961@users.noreply.github.com>
2024-04-05 17:32:45 +0900

    UI에서 수집한 오리 회전시키기 구현

--- Assets/Scripts/UIObject.cs
+++ Assets/Scripts/UIObject.cs
@@ -6,15 +6,27 @@ using UnityEngine.EventSystems;
 
 public class UIObject : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    private Vector3 pointerOffset;
+    public Quaternion initRot;
+    public float rotateSpeed = 50;
+
+    private void Awake()
+    {
+        initRot = transform.rotation;
+    }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.Rotate(Input.mousePosition.y * Time.deltaTime, Input.mousePosition.x * Time.deltaTime, 0);
+        float x = eventData.delta.x * Time.deltaTime * rotateSpeed;
+        float y = eventData.delta.y * Time.deltaTime * rotateSpeed;
+
+        transform.Rotate(0, -x, 0, Space.World);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        transform.Rotate(Vector3.zero);
+        // 다시 맨 처음 상태로 되돌리기
+        float x = (transform.rotation.y - initRot.y) * Time.deltaTime * rotateSpeed;
+
+        transform.Rotate(0, x, 0, Space.World);
     }
 }
